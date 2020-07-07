2b824cf0c05908b83ab2e237ce6506517e6261f8
Audhy <audhy.vk01@gmail.com>
2020-07-07 17:05:08 +0700

    Initial Commit

---
+++ OCR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OCR
+//
+//  Created by Audhy Virabri Kressa on 07/07/20.
+//  Copyright © 2020 Audhy Virabri Kressa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
